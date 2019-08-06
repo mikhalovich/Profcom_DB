ae4c45504c9ddfd6c825f9e357790ede048265b1
Nikita Rusilko <nikita13535@gmail.com>
2019-08-06 17:12:49 +0300

    Login page created

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="grid-center">
+    <router-view />
+  </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <form class="inputForm container z-depth-3">
+    <div class="row">
+      <div class="input-field col s10 offset-s1">
+        <input type="email" id="email" required class="validate" />
+        <label for="email">Почта</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-field col s10 offset-s1">
+        <input type="password" id="password" required class="validate" />
+        <label for="password">Пароль</label>
+      </div>
+    </div>
+    <div class="row center">
+      <button type="submit" class="btn waves-effect btn-large">Вход</button>
+    </div>
+  </form>
+</template>
