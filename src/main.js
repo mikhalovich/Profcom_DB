4a71234a871bf9155e614f6a2fb9b62ce23f2e30
Nikita Rusilko <nikita13535@gmail.com>
2019-08-06 21:49:23 +0300

    Message plugin created

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
-import Vuelidate from 'vuelidate';
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import messagePlugin from "./utils/message.plugin";
 import "./registerServiceWorker";
 import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
 Vue.use(Vuelidate);
+Vue.use(messagePlugin);
 new Vue({
   router,
   store,
