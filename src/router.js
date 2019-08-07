fb65298b3da7f3c78d8b1f3795ba9c3df23167eb
Nikita Rusilko <nikita13535@gmail.com>
2019-08-07 21:42:26 +0300

    Сreated and imported home page

--- src/router.js
+++ src/router.js
@@ -11,7 +11,8 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      meta: { layout: "home" },
+      component: () => import("./views/Home.vue")
     },
     {
       path: "/login",
