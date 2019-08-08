01bb069361fbb1cf15acacb7956677b0aba94d65
Nikita Rusilko <nikita13535@gmail.com>
2019-08-08 10:20:40 +0300

    Navbar and sidebar moved to components

---
+++ src/components/homePage/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="sidenav" id="mobile-demo">
+    <li>
+      <a class="modal-trigger waves-effect" href="#modal1">
+        <i class="material-icons left">search</i>Поиск
+      </a>
+    </li>
+    <li>
+      <a class="waves-effect" href="#">
+        <i class="material-icons left">arrow_upward</i>Выгрузить базу
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="material-icons left">autorenew</i>Обновить базу
+      </a>
+    </li>
+    <li>
+      <a href="input.html">
+        <i class="material-icons left">exit_to_app</i>Выход
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  mounted() {
+    M.Sidenav.init(document.querySelectorAll(".sidenav"), {});
+  }
+};
+</script>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,72 +1,18 @@
 <template>
   <div>
-    <div class="navbar-fixed">
-      <nav>
-        <div class="nav-wrapper">
-          <div class="container">
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-              <i class="material-icons">menu</i>
-            </a>
-
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li>
-                <a class="modal-trigger waves-effect" href="#modal1">
-                  <i class="material-icons left">search</i>Поиск
-                </a>
-              </li>
-              <li>
-                <a class="waves-effect" href="#">
-                  <i class="material-icons left">arrow_upward</i>Выгрузить базу
-                </a>
-              </li>
-              <li>
-                <a class="waves-effect" href="#">
-                  <i class="material-icons left">autorenew</i>Обновить
-                  базу
-                </a>
-              </li>
-              <li>
-                <a class="waves-effect" href="input.html">
-                  <i class="material-icons left">exit_to_app</i>Выход
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-
-    <ul class="sidenav" id="mobile-demo">
-      <li>
-        <a class="modal-trigger waves-effect" href="#modal1">
-          <i class="material-icons left">search</i>Поиск
-        </a>
-      </li>
-      <li>
-        <a class="waves-effect" href="#">
-          <i class="material-icons left">arrow_upward</i>Выгрузить базу
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i class="material-icons left">autorenew</i>Обновить базу
-        </a>
-      </li>
-      <li>
-        <a href="input.html">
-          <i class="material-icons left">exit_to_app</i>Выход
-        </a>
-      </li>
-    </ul>
-
+    <Navbar />
+    <Sidebar />
     <router-view />
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/homePage/Navbar";
+import Sidebar from "@/components/homePage/Sidebar";
 export default {
-  mounted() {
-    M.Sidenav.init(document.querySelectorAll(".sidenav"), {});
+  components: {
+    Navbar,
+    Sidebar
   }
 };
 </script>
