6fe55ba19b5180c2c0598c1de7d8213d5ad03f40
Nikita Rusilko <nikita13535@gmail.com>
2019-08-10 22:55:32 +0300

    Page of search created

---
+++ src/views/Search.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <SearchFloatingButton />
+    <div class="row">
+      <i class="material-icons left md-40">search</i>
+      <h4>Поиск студентов по критериям</h4>
+    </div>
+
+    <ul class="collapsible popout">
+      <li>
+        <div class="collapsible-header">Основная информация о студенте</div>
+        <div class="collapsible-body">
+          <BasicInformation />
+        </div>
+      </li>
+      <li>
+        <div class="collapsible-header">Дополнительная информация о студенте</div>
+        <div class="collapsible-body">
+          <AdditionalInformation />
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SearchFloatingButton from "@/components/searchPage/SearchFloatingButton";
+import BasicInformation from "../components/searchPage/items/BasicInformation";
+import AdditionalInformation from "../components/searchPage/items/AdditionalInformation";
+export default {
+  mounted() {
+    M.Collapsible.init(document.querySelectorAll(".collapsible"), {});
+    M.FormSelect.init(document.querySelectorAll("select"), {});
+    M.Datepicker.init(document.querySelectorAll(".datepicker"), {});
+  },
+  components: {
+    SearchFloatingButton,
+    BasicInformation,
+    AdditionalInformation
+  }
+};
+</script>
