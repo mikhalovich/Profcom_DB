3df200a89fc8b8c80fd1dd1f8162b421253d7099
Nikita Rusilko <nikita13535@gmail.com>
2019-08-11 22:40:48 +0300

    Routers protected

--- src/router.js
+++ src/router.js
@@ -1,17 +1,18 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import firebase from "firebase/app";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
       name: "home",
-      meta: { layout: "home" },
+      meta: { layout: "home", auth: true },
       component: () => import("./views/Home.vue")
     },
     {
@@ -23,14 +24,25 @@ export default new Router({
     {
       path: "/detail",
       name: "detail",
-      meta: { layout: "home" },
+      meta: { layout: "home", auth: true },
       component: () => import("./views/Detail.vue")
     },
     {
       path: "/search",
       name: "search",
-      meta: { layout: "home" },
+      meta: { layout: "home", auth: true },
       component: () => import("./views/Search.vue")
     }
   ]
 });
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requireAuth = to.matched.some(record => record.meta.auth);
+  if (requireAuth && !currentUser) {
+    next("/login?message=login");
+  } else {
+    next();
+  }
+});
+
+export default router;
