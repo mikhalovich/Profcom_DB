b0c5b6c97ad52da53ea10c6270c67ca5fa1ad79d
mikhalovich <z-m@tut.by>
2019-08-12 23:11:28 +0300

    after merge

---
+++ src/components/ReturnFloatingButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed-action-btn">
+    <router-link class="btn-floating btn-large waves-effect waves-light purple darken-2" to="/">
+      <i class="material-icons">keyboard_backspace</i>
+    </router-link>
+  </div>
+</template>
